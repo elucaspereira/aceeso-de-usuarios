c499492f01f6135f275e6e6d2daad89bffa0f344
Lucas Pereira <lucas_rm35@hotmail.com>
2021-12-08 22:52:19 -0300

    commit
    criando um controle de acesso de usuarios

---
+++ acesso de usuarios.py
@@ -0,0 +1,6 @@
+print('Bem vindo ao sistema')
+print('você ira cadastrar usuarios para utilizar seu equipamento')
+nome = str(input('Digite o nome do colaborador: '))
+cpf = input('Digite o cpf do colaborador: ')
+email = input('Digite o e-mail do colaborador: ')
+
